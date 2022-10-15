f7bc50159d154e37d3acc529164f9528f8cd747d
Gilherme A Halal <gilhermehalal@gmail.com>
2022-10-15 16:50:36 -0300

    atualizando - aula 3 ok

--- Python: avançando na linguagem/jogos/forca.py
+++ Python: avançando na linguagem/jogos/forca.py
@@ -3,6 +3,8 @@ def jogar():
     print("Bem vindo ao jogo da Forca!")
     print("*********************************")
     palavra_secreta = "banana"
+    letras_acertada = list("_" * len(palavra_secreta))
+    print(letras_acertada)
     enforcou = False
     acertou = False
     while not enforcou and not acertou:
@@ -11,9 +13,11 @@ def jogar():
         index=0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print('Encontrei a letra "{}" na posição {}'.format(chute, index))
-            index = index+1
+                letras_acertada[index] = letra
+                #print('Encontrei a letra "{}" na posição {}'.format(chute, index))
 
+            index = index+1
+        print(letras_acertada)
         print("jogando")
 
     print("Fim do jogo")
